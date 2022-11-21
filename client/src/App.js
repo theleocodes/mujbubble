c74405e8f3de699ab2a5711fa2fed3cb1413699a
Aditya Sharma <adityashf7@gmail.com>
2022-11-22 02:37:01 +0530

    First commit for the login/sign-up page

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Calling Components
+import Login from './components/account/Login';
+
+function App() {
+  return (
+    <div style={{marginTop: 64}}>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
