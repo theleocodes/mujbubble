1e66eac5f9022b15f5e3e6b12ff9cc9d1c599245
Aditya Sharma <adityashf7@gmail.com>
2022-12-19 22:58:34 +0530

    Updated the code to use the new API

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 import Connection from "./database/db.js";
+import router from "./routes/routes.js";
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/',router);
 const PORT = 8000;
 
 const username = process.env.DB_USERNAME;
